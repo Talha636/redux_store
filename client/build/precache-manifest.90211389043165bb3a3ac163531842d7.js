4db74e06054a84efe19bd20af3261b80a2fce824
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-11-24 20:32:25 -0500

    files added

---
+++ client/build/precache-manifest.90211389043165bb3a3ac163531842d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a72360e8d72fb65377346786b94f3bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd4987a9c6f52e0d011",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "57d65bd1a116a2c36759",
+    "url": "/static/js/2.e0512a7f.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e0512a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd4987a9c6f52e0d011",
+    "url": "/static/js/main.3d9cafdb.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
